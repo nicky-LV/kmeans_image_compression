276d0199d36bb19d7643d4723e822805daee85ef
nicky-LV <nickystnkvs@outlook.com>
2022-08-14 19:49:35 +0100

    [main.py] endpoint for uploading user images [index.tsx] rough interface for uploading image

---
+++ api/main.py
@@ -0,0 +1,21 @@
+from fastapi import FastAPI, UploadFile
+from fastapi.middleware.cors import CORSMiddleware
+from utils import *
+
+app = FastAPI()
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+@app.post("/uploadImage")
+async def upload_image(file: UploadFile):
+    # Saves the image
+    save_image(file.file.read(), file.filename)
